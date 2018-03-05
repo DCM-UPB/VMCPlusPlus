2c03d68d438aaa05866c2f3e5ad082c6a4ce065c
francesco086 <francesco086@gmail.com>
2018-03-05 16:55:39 +0100

    changed naming from lowf to divbywf for internal variables

--- src/WaveFunction.hpp
+++ src/WaveFunction.hpp
@@ -33,11 +33,11 @@ protected:
     int _npart;
     int _nspacedim;
 
-    double * _d1_logwf;
-    double * _d2_logwf;
-    double * _vd1_logwf;
-    double ** _d1vd1_logwf;
-    double ** _d2vd1_logwf;
+    double * _d1_divbywf;
+    double * _d2_divbywf;
+    double * _vd1_divbywf;
+    double ** _d1vd1_divbywf;
+    double ** _d2vd1_divbywf;
 
 public:
     WaveFunction(const int &nspacedim, const int &npart, const int &ncomp, const int &nvp, bool flag_vd1=true, bool flag_d1vd1=true, bool flag_d2vd1=true):
@@ -47,43 +47,43 @@ public:
         _npart=npart;
         _nvp=nvp;
 
-        _d1_logwf = new double[nspacedim*npart];
-        _d2_logwf = new double[nspacedim*npart];
+        _d1_divbywf = new double[nspacedim*npart];
+        _d2_divbywf = new double[nspacedim*npart];
 
-        _vd1_logwf = 0;
+        _vd1_divbywf = 0;
         if (flag_vd1){
-            _vd1_logwf = new double[nvp];
+            _vd1_divbywf = new double[nvp];
         }
 
-        _d1vd1_logwf = 0;
+        _d1vd1_divbywf = 0;
         if (flag_d1vd1){
-            _d1vd1_logwf = new double*[nspacedim*npart];
-            for (int id1=0; id1<nspacedim*npart; ++id1) _d1vd1_logwf[id1] = new double[nvp];
+            _d1vd1_divbywf = new double*[nspacedim*npart];
+            for (int id1=0; id1<nspacedim*npart; ++id1) _d1vd1_divbywf[id1] = new double[nvp];
         }
 
-        _d2vd1_logwf = 0;
+        _d2vd1_divbywf = 0;
         if (flag_d2vd1){
-            _d2vd1_logwf = new double*[nspacedim*npart];
-            for (int id2=0; id2<nspacedim*npart; ++id2) _d2vd1_logwf[id2] = new double[nvp];
+            _d2vd1_divbywf = new double*[nspacedim*npart];
+            for (int id2=0; id2<nspacedim*npart; ++id2) _d2vd1_divbywf[id2] = new double[nvp];
         }
     }
     virtual ~WaveFunction(){
-        delete[] _d1_logwf;
-        delete[] _d2_logwf;
-        if (_vd1_logwf != 0){
-            delete[] _vd1_logwf;
+        delete[] _d1_divbywf;
+        delete[] _d2_divbywf;
+        if (_vd1_divbywf != 0){
+            delete[] _vd1_divbywf;
         }
-        if (_d1vd1_logwf != 0){
+        if (_d1vd1_divbywf != 0){
             for (int id1=0; id1<getTotalNDim(); ++id1){
-                delete[] _d1vd1_logwf[id1];
+                delete[] _d1vd1_divbywf[id1];
             }
-            delete[] _d1vd1_logwf;
+            delete[] _d1vd1_divbywf;
         }
-        if (_d2vd1_logwf != 0){
+        if (_d2vd1_divbywf != 0){
             for (int id2=0; id2<getTotalNDim(); ++id2){
-                delete[] _d2vd1_logwf[id2];
+                delete[] _d2vd1_divbywf[id2];
             }
-            delete[] _d2vd1_logwf;
+            delete[] _d2vd1_divbywf;
         }
     }
 
@@ -115,23 +115,23 @@ public:
 
     // --- getters and setters for the derivatives
     // first derivative divided by the wf
-    void setD1DivByWF(const int &id1, const double &d1_logwf){_d1_logwf[id1] = d1_logwf;}
-    double getD1DivByWF(const int &id1){return _d1_logwf[id1];}
+    void setD1DivByWF(const int &id1, const double &d1_divbywf){_d1_divbywf[id1] = d1_divbywf;}
+    double getD1DivByWF(const int &id1){return _d1_divbywf[id1];}
     // second derivative divided by the wf
-    void setD2DivByWF(const int &id2, const double &d2_logwf){_d2_logwf[id2] = d2_logwf;}
-    double getD2DivByWF(const int &id2){return _d2_logwf[id2];}
+    void setD2DivByWF(const int &id2, const double &d2_divbywf){_d2_divbywf[id2] = d2_divbywf;}
+    double getD2DivByWF(const int &id2){return _d2_divbywf[id2];}
     // variational derivative divided by the wf
-    bool hasVD1(){return (_vd1_logwf == 0 ? false : true);}
-    void setVD1DivByWF(const int &ivd1, const double &vd1_logwf){_vd1_logwf[ivd1] = vd1_logwf;}
-    double getVD1DivByWF(const int &ivd1){return _vd1_logwf[ivd1];}
+    bool hasVD1(){return (_vd1_divbywf == 0 ? false : true);}
+    void setVD1DivByWF(const int &ivd1, const double &vd1_divbywf){_vd1_divbywf[ivd1] = vd1_divbywf;}
+    double getVD1DivByWF(const int &ivd1){return _vd1_divbywf[ivd1];}
     // cross derivative: first derivative and first variational derivative divided by the wf
-    bool hasD1VD1(){return (_d1vd1_logwf == 0 ? false : true);}
-    void setD1VD1DivByWF(const int &id1, const int &ivd1, const double &d1vd1_logwf){_d1vd1_logwf[id1][ivd1] = d1vd1_logwf;}
-    double getD1VD1DivByWF(const int &id1, const int &ivd1){return _d1vd1_logwf[id1][ivd1];}
+    bool hasD1VD1(){return (_d1vd1_divbywf == 0 ? false : true);}
+    void setD1VD1DivByWF(const int &id1, const int &ivd1, const double &d1vd1_divbywf){_d1vd1_divbywf[id1][ivd1] = d1vd1_divbywf;}
+    double getD1VD1DivByWF(const int &id1, const int &ivd1){return _d1vd1_divbywf[id1][ivd1];}
     // cross derivative: second derivative and first variational derivative divided by the wf
-    bool hasD2VD1(){return (_d2vd1_logwf == 0 ? false : true);}
-    void setD2VD1DivByWF(const int &id2, const int &ivd1, const double &d2vd1_logwf){_d2vd1_logwf[id2][ivd1] = d2vd1_logwf;}
-    double getD2VD1DivByWF(const int &id2, const int &ivd1){return _d2vd1_logwf[id2][ivd1];}
+    bool hasD2VD1(){return (_d2vd1_divbywf == 0 ? false : true);}
+    void setD2VD1DivByWF(const int &id2, const int &ivd1, const double &d2vd1_divbywf){_d2vd1_divbywf[id2][ivd1] = d2vd1_divbywf;}
+    double getD2VD1DivByWF(const int &id2, const int &ivd1){return _d2vd1_divbywf[id2][ivd1];}
 };
 
 
