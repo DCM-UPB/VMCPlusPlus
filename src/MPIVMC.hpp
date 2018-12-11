2a15c2959a1a976d9a7690e0dc91c93155e33072
Jan Kessler <jakessle@t-online.de>
2018-12-11 21:47:36 +0100

    use random initialization per CPU before every MPI-MC integration, currently made happen implicitly

--- src/MPIVMC.hpp
+++ src/MPIVMC.hpp
@@ -49,8 +49,11 @@ namespace MPIVMC
         #endif
     }
 
-    void Integrate(MCI * const mci, const long &Nmc, double * average, double * error, int NfindMRT2stepIterations, int NdecorrelationSteps, int nblocks=-1)
+    void Integrate(MCI * const mci, const long &Nmc, double * average, double * error, int NfindMRT2stepIterations, int NdecorrelationSteps, bool randomizeWalkers = false, int nblocks=-1)
     {
+        if (randomizeWalkers) {
+            mci->newRandomX();
+        }
         #if USE_MPI==1
         MPIMCI::integrate(mci, Nmc, average, error, NfindMRT2stepIterations, NdecorrelationSteps, nblocks < 0 ? 16 : nblocks); // if compiling with USE_MPI, set default block count to 16
         #else
@@ -64,11 +67,11 @@ namespace MPIVMC
         // if compiling with USE_MPI, set fixed defaults (totaling 5000 quick no-sample steps)
         int stepsMRT2 = findMRT2step ? 25 : 0;
         int stepsDecorr = initialdecorrelation ? 2500 : 0;
-        Integrate(mci, Nmc, average, error, stepsMRT2, stepsDecorr, nblocks < 0 ? 16 : nblocks); // if compiling with USE_MPI, set fixed defaults (totaling 5000 quick no-sample steps)
+        Integrate(mci, Nmc, average, error, stepsMRT2, stepsDecorr, true, nblocks < 0 ? 16 : nblocks); // if compiling with USE_MPI, also use random initial walker positions and fixed blocking
         #else
         int stepsMRT2 = findMRT2step ? -1 : 0;
         int stepsDecorr = initialdecorrelation ? -1 : 0;
-        Integrate(mci, Nmc, average, error, stepsMRT2, stepsDecorr, nblocks < 0 ? 0 : nblocks);
+        Integrate(mci, Nmc, average, error, stepsMRT2, stepsDecorr, false, nblocks < 0 ? 0 : nblocks);
         #endif
 
     }
