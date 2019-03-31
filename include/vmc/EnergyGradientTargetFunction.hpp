9636ad765e1e5de06e75b4d37fb25d44818ea2bf
Jan Kessler <jakessle@t-online.de>
2019-03-31 04:34:18 +0200

    adapt EnergyGradientTargetFunction to new NFM

--- include/vmc/EnergyGradientTargetFunction.hpp
+++ include/vmc/EnergyGradientTargetFunction.hpp
@@ -22,16 +22,16 @@ protected:
 
 public:
     EnergyGradientTargetFunction(WaveFunction * wf, Hamiltonian * H, const int &E_Nmc, const int &grad_E_Nmc, mci::MCI * mci, const double &lambda_reg = 0.):
-            nfm::NoisyFunctionWithGradient(wf->getNVP()),
+            nfm::NoisyFunctionWithGradient(wf->getNVP(), true),
             _wf(wf), _H(H), _mci(mci), _E_Nmc(E_Nmc), _grad_E_Nmc(grad_E_Nmc), _lambda_reg(lambda_reg) {}
 
     ~EnergyGradientTargetFunction() override = default;
 
 
     // NoisyFunctionWithGradient implementation
-    void f(const double * vp, double &f, double &df) override;
-    void grad(const double * vp, double * grad_E, double * dgrad_E) override;
-    void fgrad(const double * vp, double &f, double &df, double * grad_E, double * dgrad_E) override;
+    nfm::NoisyValue f(const std::vector<double> &vp) override;
+    void grad(const std::vector<double> &vp, std::vector<nfm::NoisyValue> &grad) override;
+    nfm::NoisyValue fgrad(const std::vector<double> &vp, std::vector<nfm::NoisyValue> &grad) override;
 };
 } // namespace vmc
 
--- src/EnergyGradientTargetFunction.cpp
+++ src/EnergyGradientTargetFunction.cpp
@@ -6,42 +6,42 @@
 namespace vmc
 {
 
-void EnergyGradientTargetFunction::f(const double * vp, double &f, double &df)
+nfm::NoisyValue EnergyGradientTargetFunction::f(const std::vector<double> &vp)
 {
     // set the variational parameters given as input
-    _wf->setVP(vp);
+    _wf->setVP(vp.data());
     // perform the integral and store the values
     double obs[4];
     double dobs[4];
     MPIVMC::Integrate(_mci, _E_Nmc, obs, dobs, true, true);
-    f = obs[0];
-    df = dobs[0];
+    nfm::NoisyValue f{obs[0], dobs[0]};
 
     if (_lambda_reg > 0.) { // compute the regularization term
-        double norm = 0.;
-        for (int i = 0; i < _wf->getNVP(); ++i) { norm += vp[i]*vp[i]; }
-        f += _lambda_reg*norm/_wf->getNVP();
+        const double norm = std::inner_product(vp.begin(), vp.end(), vp.begin(), 0.);
+        f.value += _lambda_reg*norm/_wf->getNVP();
     }
+    
+    return f;
 }
 
 
-void EnergyGradientTargetFunction::grad(const double * vp, double * grad_E, double * dgrad_E)
+void EnergyGradientTargetFunction::grad(const std::vector<double> &vp, std::vector<nfm::NoisyValue> &grad)
 {
-    double f, df;
-    fgrad(vp, f, df, grad_E, dgrad_E);
+    fgrad(vp, grad);
 }
 
 
-void EnergyGradientTargetFunction::fgrad(const double * vp, double &f, double &df, double * grad_E, double * dgrad_E)
+nfm::NoisyValue EnergyGradientTargetFunction::fgrad(const std::vector<double> &vp, std::vector<nfm::NoisyValue> &grad)
 {
     // set the variational parameters given as input
-    _wf->setVP(vp);
+    _wf->setVP(vp.data());
     // add gradient obs to MCI
     _mci->addObservable(EnergyGradientMCObservable(_wf, _H), 1, 1, false, true); // skipping equlibiration for gradients
     // perform the integral and store the values
     double obs[4 + 2*_wf->getNVP()];
     double dobs[4 + 2*_wf->getNVP()];
     MPIVMC::Integrate(_mci, _grad_E_Nmc, obs, dobs, true, true);
+    _mci->popObservable(); // remove the gradient obs (it will be deleted)
     // create pointers for ease of use and readability
     const double * const H = obs;
     const double * const dH = dobs;
@@ -49,18 +49,20 @@ void EnergyGradientTargetFunction::fgrad(const double * vp, double &f, double &d
     const double * const dOi = dobs + 4;
     const double * const HOi = obs + 4 + _wf->getNVP();
     const double * const dHOi = dobs + 4 + _wf->getNVP();
-    f = H[0];
-    df = dH[0];
+    nfm::NoisyValue f{H[0], dH[0]};
     // compute direction (or gradient) to follow
     for (int i = 0; i < _wf->getNVP(); ++i) {
-        grad_E[i] = 2.*(HOi[i] - H[0]*Oi[i]);
-        dgrad_E[i] = 2.*(dHOi[i] + fabs(H[0]*Oi[i])*(dH[0]/H[0] + dOi[i]/Oi[i]));
+        grad[i].value = 2.*(HOi[i] - H[0]*Oi[i]);
+        grad[i].error = 2.*(dHOi[i] + fabs(H[0]*Oi[i])*(dH[0]/H[0] + dOi[i]/Oi[i]));
     }
 
-    if (_lambda_reg > 0.) { // compute the regularization derivative
-        const double fac = 2.*_lambda_reg/_wf->getNVP();
-        for (int i = 0; i < _wf->getNVP(); ++i) { grad_E[i] += fac*vp[i]; }
+    if (_lambda_reg > 0.) { // compute the regularization terms
+        const double norm = std::inner_product(vp.begin(), vp.end(), vp.begin(), 0.);
+        const double fac = _lambda_reg/_wf->getNVP();
+        f.value += fac*norm;
+        for (int i = 0; i < _wf->getNVP(); ++i) { grad[i].value += 2.*fac*vp[i]; }
     }
-    _mci->popObservable(); // remove the gradient obs (it will be deleted)
+
+    return f;
 }
 } // namespace vmc
