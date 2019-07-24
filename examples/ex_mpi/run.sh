73661ffc4137c9e47c77b9e7d9ab9f30865adce0
Jan Kessler <jakessle@t-online.de>
2019-07-24 13:52:09 +0200

    Fixed if-condition in the mpi example run script

--- examples/ex_mpi/run.sh
+++ examples/ex_mpi/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ../../build/examples
-if [ "$1" = "" ]; then
+if [[ "$1" == "" ]]; then
     echo "!----------------------------------------------------------------------!"
     echo "! Warning: Number of threads not specified -> running single-threaded. !"
     echo "! To use parallel MC integration, pass the #threads: e.g. ./run.sh 2   !"
